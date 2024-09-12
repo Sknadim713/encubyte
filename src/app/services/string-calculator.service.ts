43701c68b09a004296d04ead4af384478fcd9678
shaikh <sknadeem@>
2024-09-12 22:15:05 +0530

    method handle single and two comma

--- src/app/services/string-calculator.service.ts
+++ src/app/services/string-calculator.service.ts
@@ -8,11 +8,15 @@ export class StringCalculatorService {
   constructor() { }
 
   add(numbers: string): number {
-    console.log(numbers);
-
     if (!numbers) {
       return 0;
     }
-    return 0;
+
+    const numArray = numbers.split(',');
+    const sum = numArray.reduce((acc, num) => acc + parseInt(num, 10), 0);
+    console.log("check sum", sum);
+
+    return sum;
   }
+
 }
