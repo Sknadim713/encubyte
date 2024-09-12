26a7e7b835e713c44db2bb9be73e8c7459b1ad26
shaikh <sknadeem@>
2024-09-12 22:11:11 +0530

    comma seprate number

--- src/app/services/string-calculator.service.spec.ts
+++ src/app/services/string-calculator.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { StringCalculatorService } from './string-calculator.service';
 
-describe('StringCalculatorService', () => {
+describe('Calculator Service', () => {
   let service: StringCalculatorService;
 
   beforeEach(() => {
@@ -9,7 +9,16 @@ describe('StringCalculatorService', () => {
     service = TestBed.inject(StringCalculatorService);
   });
 
-  it('should return 0 for an empty string', () => {
+  it('should return 0 ', () => {
     expect(service.add('')).toEqual(0);
   });
+
+  it('should return the number itself when one number is provided', () => {
+    expect(service.add('1')).toEqual(1);
+  });
+
+  it('should return the sum of two comma-separated numbers', () => {
+    expect(service.add('1,2')).toEqual(3);
+  });
+
 });
